8e698efb0c0a775f43cebb268844052bd7d34e6b
Phatsakorn Chosungnoen <99648810+NNJio@users.noreply.github.com>
2022-08-17 22:22:11 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'newZaEiEI',
+      title: 'This test git hub',
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
